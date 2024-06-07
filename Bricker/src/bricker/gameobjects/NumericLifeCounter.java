b652cf8135529ec55b197198805a658110b9c0f0
amirt97 <amir.tuboul@mail.huji.ac.il>
2024-06-07 16:04:32 +0300

    Added numeric life counter :)

--- Bricker/src/bricker/gameobjects/NumericLifeCounter.java
+++ Bricker/src/bricker/gameobjects/NumericLifeCounter.java
@@ -3,10 +3,20 @@ package Bricker.src.bricker.gameobjects;
 
 import danogl.GameObject;
 import danogl.gui.rendering.Renderable;
+import danogl.gui.rendering.TextRenderable;
+import danogl.util.Counter;
 import danogl.util.Vector2;
 
-public class NumericLifeCounter extends GameObject {
+import java.awt.*;
 
+public class NumericLifeCounter extends GameObject {
+    private static final int GREEN = 3;
+    private static final int YELLOW = 2;
+    private static final int RED = 1;
+
+    private final Counter livesCounter;
+    private final TextRenderable txtRenderer;
+    private int currentLives;
     /**
      * Construct a new GameObject instance.
      *
@@ -16,7 +26,42 @@ public class NumericLifeCounter extends GameObject {
      * @param renderable    The renderable representing the object. Can be null, in which case
      *                      the GameObject will not be rendered.
      */
-    public NumericLifeCounter(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
-        super(topLeftCorner, dimensions, renderable);
+    public NumericLifeCounter(Counter livesCounter,Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
+        super(topLeftCorner, dimensions, null);
+        txtRenderer = new TextRenderable(Integer.toString(livesCounter.value()));
+        txtRenderer.setColor(Color.green);
+        this.renderer().setRenderable(txtRenderer);
+        this.livesCounter = livesCounter;
+        this.currentLives = livesCounter.value();
+
+
     }
+
+    /**
+     * Update the numeric lives according to current lives count.
+     * @param deltaTime The time elapsed, in seconds, since the last frame. Can
+     *                  be used to determine a new position/velocity by multiplying
+     *                  this delta with the velocity/acceleration respectively
+     *                  and adding to the position/velocity:
+     *                  velocity += deltaTime*acceleration
+     *                  pos += deltaTime*velocity
+     */
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if (currentLives != livesCounter.value()){
+            txtRenderer.setString(Integer.toString(livesCounter.value()));
+            if (livesCounter.value() >= GREEN){
+                txtRenderer.setColor(Color.green);
+            }
+            else if(livesCounter.value() == YELLOW){
+                txtRenderer.setColor(Color.yellow);
+            }
+            else if(livesCounter.value() == RED){
+                txtRenderer.setColor(Color.red);
+            }
+            currentLives = livesCounter.value();
+        }
+    }
+
 }
