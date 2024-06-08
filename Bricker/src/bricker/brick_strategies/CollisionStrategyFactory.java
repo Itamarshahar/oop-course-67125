277e6a6bc11cfe30d71ea7f714011cfa73773347
Itamar Shahar <itamarshahar2@gmail.com>
2024-06-08 17:41:55 +0300

    adding the Puck Strategy class and the CollisionStrategyFactory

---
+++ Bricker/src/bricker/brick_strategies/CollisionStrategyFactory.java
@@ -0,0 +1,28 @@
+package Bricker.src.bricker.brick_strategies;
+
+import Bricker.src.bricker.gameobjects.Puck;
+import Bricker.src.bricker.main.BrickerGameManager;
+
+public class CollisionStrategyFactory {
+    private final BrickerGameManager objectCollection;
+
+    public CollisionStrategyFactory(BrickerGameManager objectCollection) {
+        this.objectCollection = objectCollection;
+    }
+
+
+    public CollisionStrategy getCollisionStrategy(CollisionStrategy baseStrategy,
+                                                  int strategy) {
+
+        if (baseStrategy == null) {
+            baseStrategy = new BasicCollisionStrategy(objectCollection);
+        }
+        switch (strategy) {
+            case 0:
+                return new PuckStrategy(objectCollection);
+
+            default:
+                return baseStrategy;
+        }
+    }
+}
---
+++ Bricker/src/bricker/brick_strategies/PuckStrategy.java
@@ -0,0 +1,15 @@
+package Bricker.src.bricker.brick_strategies;
+
+import Bricker.src.bricker.main.BrickerGameManager;
+import danogl.GameObject;
+
+public class PuckStrategy implements CollisionStrategy {
+    public PuckStrategy(BrickerGameManager objectCollection) {
+
+    }
+
+    @Override
+    public void onCollision(GameObject obj1, GameObject obj2) {
+        // TODO
+    }
+}
