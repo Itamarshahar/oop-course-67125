8d0469da8bb4db524afa518b317aae5c94f5addb
Itamar Shahar <itamarshahar2@gmail.com>
2024-06-10 09:33:42 +0300

    fix the Heart calls to support multiple Hearts

--- Bricker/src/bricker/gameobjects/Heart.java
+++ Bricker/src/bricker/gameobjects/Heart.java
@@ -1,15 +1,11 @@
 package Bricker.src.bricker.gameobjects;
 
-import danogl.GameManager;
+import Bricker.src.bricker.main.BrickerGameManager;
 import danogl.GameObject;
 import danogl.collisions.Collision;
-import danogl.collisions.GameObjectCollection;
-import danogl.gui.WindowController;
 import danogl.gui.rendering.Renderable;
-import danogl.util.Counter;
 import danogl.util.Vector2;
 
-import static Bricker.src.bricker.main.BrickerGameManager.MAX_NUM_LIFE;
 import static Bricker.src.bricker.main.BrickerGameManager.ORIGINAL_PADDLE_TAG;
 //import src.bricker.main.BrickerGameManager;
 //import src.brick_strategies.HeartStrategy;
@@ -19,20 +15,23 @@ public class Heart extends GameObject {
      * Construct a new GameObject instance.
      *
      * @param topLeftCorner Position of the object, in window coordinates (pixels).
-     *                      Note that (0,0) is the top-left corner of the window.
+     * Note that (0,0) is the top-left corner of the window.
      * @param dimensions    Width and height in window coordinates.
      * @param renderable    The renderable representing the object. Can be null, in which case
-     *                      the GameObject will not be rendered.
+     * the GameObject will not be rendered.
      * @param gameObjects all the objects on the window
      * @param livesCounter counts the number of the users lives
      * @param used to get the dimensoin of the window, for removing the heart from the gameObjects when
-     *             it is outside the display
+     * it is outside the display
      */
 
-    static private GameObjectCollection gameObjects;
-    static private Counter livesCounter;
+//    private final GameObjectCollection gameObjects;
+//    private final Counter livesCounter;
+//    private final WindowController windowController;
+    private final String paddleTag;
+    private final float minY;
+    private final BrickerGameManager brickerGameManager;
 
-    static private WindowController windowController;
 
     /**
      * constructor
@@ -40,68 +39,71 @@ public class Heart extends GameObject {
     public Heart(Vector2 topLeftCorner,
                  Vector2 dimensions,
                  Renderable renderable,
-                 GameObjectCollection gameObjects,
-                 Counter livesCounter,
-                 WindowController windowController) {
+//                 GameObjectCollection gameObjects,
+//                 Counter livesCounter,
+//                 WindowController windowController,
+                 BrickerGameManager brickerGameManager,
+                 String paddleTag,
+                 float minY) {
         super(topLeftCorner, dimensions, renderable);
-        this.gameObjects = gameObjects;
-        this.livesCounter = livesCounter;
-        this.windowController = windowController;
-    }
+        this.brickerGameManager = brickerGameManager;
 
-    /**
-     *
-     * @param other The other GameObject- that we could collide with.
-     * @return boolean - true if collision will execute, false if not
-     *
-     * checks if collision should be executed
-     */
+        this.paddleTag = paddleTag;
+        this.minY = minY;
+        this.setVelocity(new Vector2(0, 100)); // Set initial downward movement
 
 
-    @Override
-    public boolean shouldCollideWith(GameObject other) {
-        if (other.getTag().equals(ORIGINAL_PADDLE_TAG)) {
-            return super.shouldCollideWith(other);
-        }
-        return false;
     }
 
 
     /**
-     *
-     * @param other The GameObject with which a collision occurred.
+     * @param other     The GameObject with which a collision occurred.
      * @param collision Information regarding this collision.
      *                  A reasonable elastic behavior can be achieved with:
      *                  setVelocity(getVelocity().flipped(collision.getNormal()));
-     *
+     *                  <p>
      *                  this function removes the heart from the board if it hits the paddle and
      *                  make increment of the users lives
      */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
-        gameObjects.removeGameObject(this);
-        if (livesCounter.value() <= MAX_NUM_LIFE){
-            livesCounter.increment();
-        }
+        brickerGameManager.increaseLifeCounter();
+        brickerGameManager.removeGameObjectHandler(this);
+
+
     }
 
     /**
-     *
+     * @param other The other GameObject- that we could collide with.
+     * @return boolean - true if collision will execute, false if not
+     * <p>
+     * checks if collision should be executed
+     */
+    @Override
+    public boolean shouldCollideWith(GameObject other) {
+        return (other.getTag().equals(ORIGINAL_PADDLE_TAG));// {
+//            return super.shouldCollideWith(other);
+//        }
+//        return false;
+    }
+
+    /**
      * @param deltaTime The time elapsed, in seconds, since the last frame. Can
      *                  be used to determine a new position/velocity by multiplying
      *                  this delta with the velocity/acceleration respectively
      *                  and adding to the position/velocity:
      *                  velocity += deltaTime*acceleration
      *                  pos += deltaTime*velocity
-     *
-     * used to remove the heart from the window when it gets outside the display
+     *                  <p>
+     *                  used to remove the heart from the window when it gets outside the display
      */
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        if (this.getCenter().y() > windowController.getWindowDimensions().y()){
-            gameObjects.removeGameObject(this);
+//        if (this.getCenter().y() > windowController.getWindowDimensions().y()) {
+        if (this.getCenter().y() > minY){
+            brickerGameManager.removeGameObjectHandler(this);
         }
     }
 }
