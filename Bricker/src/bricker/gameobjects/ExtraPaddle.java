1e04fbe6fc4ff243490f25e1d850a9ed0b534084
Itamar Shahar <itamarshahar2@gmail.com>
2024-06-11 14:24:16 +0300

    add documentation

--- Bricker/src/bricker/gameobjects/ExtraPaddle.java
+++ Bricker/src/bricker/gameobjects/ExtraPaddle.java
@@ -1,4 +1,5 @@
 package Bricker.src.bricker.gameobjects;
+
 import Bricker.src.bricker.main.BrickerGameManager;
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
@@ -7,34 +8,36 @@ import danogl.gui.UserInputListener;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 import danogl.util.Counter;
+
 /**
  * ExtraPaddle.java
- *
+ * <p>
  * This class represents an extra paddle in the game. It extends the Paddle class
  * and handles collisions with other objects, removing itself from the game after
  * a certain number of collisions.
  */
-public class ExtraPaddle extends Paddle{
+public class ExtraPaddle extends Paddle {
     private final BrickerGameManager gameObjectCollection;
     private final Counter collisionCounter;
     private int paddleLives = 2;
-     /**
+
+    /**
      * Construct a new ExtraPaddle instance.
      *
-     * @param topLeftCorner     Position of the paddle, in window coordinates (pixels).
-     *                          Note that (0,0) is the top-left corner of the window.
-     * @param dimensions        Width and height of the paddle in window coordinates.
-     * @param renderable        The renderable representing the paddle. Can be null, in which case
-     *                          the ExtraPaddle will not be rendered.
-     * @param inputListener     The user input listener for controlling the paddle.
-     * @param wallWidth         The width of the wall surrounding the game area.
-     * @param windowDimension   The dimensions of the game window.
+     * @param topLeftCorner        Position of the paddle, in window coordinates (pixels).
+     *                             Note that (0,0) is the top-left corner of the window.
+     * @param dimensions           Width and height of the paddle in window coordinates.
+     * @param renderable           The renderable representing the paddle. Can be null, in which case
+     *                             the ExtraPaddle will not be rendered.
+     * @param inputListener        The user input listener for controlling the paddle.
+     * @param wallWidth            The width of the wall surrounding the game area.
+     * @param windowDimension      The dimensions of the game window.
      * @param gameObjectCollection The collection of game objects to remove the paddle from.
      */
     public ExtraPaddle(Vector2 topLeftCorner, Vector2 dimensions,
                        Renderable renderable, UserInputListener inputListener, int wallWidth, Vector2 windowDimension
-                        , BrickerGameManager gameObjectCollection){
-        super(topLeftCorner,dimensions,renderable,inputListener,wallWidth,windowDimension);
+            , BrickerGameManager gameObjectCollection) {
+        super(topLeftCorner, dimensions, renderable, inputListener, wallWidth, windowDimension);
         this.collisionCounter = new Counter();
         this.gameObjectCollection = gameObjectCollection;
 
@@ -51,10 +54,10 @@ public class ExtraPaddle extends Paddle{
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
-        if (other instanceof Ball){
+        if (other instanceof Ball) {
             collisionCounter.increment();
         }
-        if(collisionCounter.value() == paddleLives){
+        if (collisionCounter.value() == paddleLives) {
             gameObjectCollection.removeGameObjectHandler(this);
         }
 
--- Bricker/src/bricker/gameobjects/Puck.java
+++ Bricker/src/bricker/gameobjects/Puck.java
@@ -1,6 +1,6 @@
 package Bricker.src.bricker.gameobjects;
 
-import  Bricker.src.bricker.main.BrickerGameManager;
+import Bricker.src.bricker.main.BrickerGameManager;
 import danogl.GameObject;
 import danogl.collisions.Collision;
 import danogl.collisions.Layer;
@@ -11,7 +11,7 @@ import danogl.collisions.Layer;
 
 /**
  * Puck.java
- *
+ * <p>
  * This class represents a puck in the game, inheriting from the Ball class.
  * It handles removing itself from the game when it goes below a certain height.
  */
@@ -23,11 +23,11 @@ public class Puck extends Ball {
     /**
      * Constructs a new Puck object, inheriting from the Ball class.
      *
-     * @param topLeftCorner     Position of the puck in window coordinates (pixels).
-     * @param dimensions        Width and height of the puck in window coordinates.
-     * @param renderable        The renderable representing the puck's appearance.
-     * @param collisionSound    The sound played when the puck collides with other objects.
-     * @param minHeight         The minimum Y-coordinate the puck can reach before being removed.
+     * @param topLeftCorner      Position of the puck in window coordinates (pixels).
+     * @param dimensions         Width and height of the puck in window coordinates.
+     * @param renderable         The renderable representing the puck's appearance.
+     * @param collisionSound     The sound played when the puck collides with other objects.
+     * @param minHeight          The minimum Y-coordinate the puck can reach before being removed.
      * @param brickerGameManager Reference to the BrickerGameManager instance for game logic interaction.
      */
     public Puck(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, Sound collisionSound,
@@ -36,7 +36,8 @@ public class Puck extends Ball {
         this.minHeight = minHeight;
         this.brickerGameManager = brickerGameManager;
     }
-/**
+
+    /**
      * Handles collisions with other objects, calling the parent class's method first.
      * No specific behavior is added in this overridden method.
      *
@@ -47,7 +48,8 @@ public class Puck extends Ball {
     public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
     }
-/**
+
+    /**
      * Retrieves the puck's collision counter from the parent class.
      *
      * @return The number of collisions the puck has been involved in.
