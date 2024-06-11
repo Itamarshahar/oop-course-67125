90e5c1c9f928e67e3930d6e345f32ca56383db65
Itamar Shahar <itamarshahar2@gmail.com>
2024-06-11 16:28:59 +0300

    add the extra paddle

--- Bricker/src/bricker/gameobjects/ExtraPaddle.java
+++ Bricker/src/bricker/gameobjects/ExtraPaddle.java
@@ -75,6 +75,4 @@ public class ExtraPaddle extends Paddle {
         }
 
     }
-
-
 }
--- Bricker/src/bricker/gameobjects/GraphicLifeCounter.java
+++ Bricker/src/bricker/gameobjects/GraphicLifeCounter.java
@@ -102,5 +102,5 @@ public class GraphicLifeCounter extends GameObject {
             numOfLives++;
         }
     }
-
+}
 
