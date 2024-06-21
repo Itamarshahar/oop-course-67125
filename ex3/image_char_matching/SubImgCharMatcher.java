4df25d279efa1dc8d843604215ab52d827631b68
Itamarshahar <ishahar@varonis.com>
2024-06-21 08:44:44 +0300

    adding SubImgCharMatcher

---
+++ ex3/image_char_matching/SubImgCharMatcher.java
@@ -0,0 +1,6 @@
+class SubImgCharMatcher {
+    public SubImgCharMatcher(char[] charset);
+    public char getCharByImageBrightness(double brightness);
+) public void addChar(char c
+    ) public void removeChar(char c
+}
