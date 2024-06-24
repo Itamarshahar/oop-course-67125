91b44ee15bb4ba737bec3f04d84dd72261f00632
Itamar Shahar <itamarshahar2@gmail.com>
2024-06-24 08:36:13 +0300

    fix warrnigs

--- ex3/src/image/GrayScaleConverter.java
+++ ex3/src/image/GrayScaleConverter.java
@@ -1,2 +1,21 @@
-package image;public class GrayScaleConverter {
+package image;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class GrayScaleConverter {
+    public static double calculateBrightness(BufferedImage image) {
+        double totalBrightness = 0.0;
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = new Color(image.getRGB(x, y));
+                double grayPixel = color.getRed() * 0.2126 + color.getGreen() * 0.7152 + color.getBlue() * 0.0722;
+                totalBrightness += grayPixel / 255;
+            }
+        }
+        return totalBrightness / (width * height);
+    }
 }
--- ex3/src/image/SubImage.java
+++ ex3/src/image/SubImage.java
@@ -1,2 +1,30 @@
-package image;public class SubImage {
+package image;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubImage {
+    private BufferedImage image;
+
+    public SubImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public List<BufferedImage> divideImage(int subImageSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        List<BufferedImage> subImages = new ArrayList<>();
+
+        for (int y = 0; y < height; y += subImageSize) {
+            for (int x = 0; x < width; x += subImageSize) {
+                int subWidth = Math.min(subImageSize, width - x);
+                int subHeight = Math.min(subImageSize, height - y);
+
+                BufferedImage subImage = image.getSubimage(x, y, subWidth, subHeight);
+                subImages.add(subImage);
+            }
+        }
+        return subImages;
+    }
 }
