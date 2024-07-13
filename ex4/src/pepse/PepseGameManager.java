745a250ce0379e55dddd575fd5158a903a0733d6
Itamar Shahar <itamarshahar2@gmail.com>
2024-07-13 18:00:31 +0300

    ex4-Terrin-wip

--- ex4/src/pepse/PepseGameManager.java
+++ ex4/src/pepse/PepseGameManager.java
@@ -7,8 +7,12 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
+
+import java.util.List;
+import java.util.Random;
 
 public class PepseGameManager extends GameManager {
     // TODO doc
@@ -22,11 +26,19 @@ public class PepseGameManager extends GameManager {
                                WindowController windowController){
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         GameObject sky = Sky.create(windowController.getWindowDimensions());
-        gameObjects().addGameObject(sky, Layer.BACKGROUND);
-        Terrain terrain = new Terrain(windowController.getWindowDimensions(),
-                0 );
-        terrain.createInRange();
+//        gameObjects().addGameObject(sky, Layer.BACKGROUND);
 
+
+        Random random = new Random();
+        Terrain terrain = new Terrain(windowController.getWindowDimensions(), random.nextInt());
+//        Terrain terrain = new Terrain(windowController.getWindowDimensions(),
+//                0 );
+        List<Block> blocks = terrain.createInRange(-5,
+                (int) windowController.getWindowDimensions().x() + 5); //
+        // TODO - itamar - verify why (-5) ?
+        for (Block block : blocks){
+            gameObjects().addGameObject(block, Layer.STATIC_OBJECTS);
+        }
     }
 }
 
--- ex4/src/pepse/world/Terrain.java
+++ ex4/src/pepse/world/Terrain.java
@@ -3,18 +3,26 @@ package pepse.world;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 import pepse.util.ColorSupplier;
-
+import pepse.util.NoiseGenerator;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Terrain {
-    private float groundHeightAtX0;
     private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
-    public Terrain(Vector2 windowDimensions, int seed){
-        this.groundHeightAtX0 = windowDimensions.x()*(2/3); // TODO verify
+    private final float groundHeightAtX0;
+    private final String GROUND_TAG = "ground";
+    private static final int TERRAIN_DEPTH = 20;
+    private final NoiseGenerator noiseGenerator;
+    private final Vector2 windowDimensions;
+    public Terrain(Vector2 windowDimensions, int seed) {
+        this.groundHeightAtX0 = windowDimensions.x() * (2 / 3); // TODO verify
         // logic
+        this.noiseGenerator = new NoiseGenerator(seed, (int) groundHeightAtX0);
+        this.windowDimensions = windowDimensions;
 
     }
+
     public float groundHeightAt(float x) {
         // TODO
         // לקראת ההגשה, על תוואי הקרקע להיות מעניין יותר. הנה דרך שלא תעבוד: גובה האדמה בכל
@@ -25,13 +33,32 @@ public class Terrain {
         //Perlin . ורבים וטובות מימשו סוגים רבים של פונקציות כאלו ,)smooth noise-function( " רעש חלקה
         //הוא אלגוריתם פופולרי לפונקציה כזו. Noise
         //Perlin Noise שבעזרתה תוכלו לייצר NoiseGenerator בקבצים המסופקים לכם תוכלו למצוא את המחלקה
-        //לפי התיעוד במחלקהגשדs
-        return groundHeightAtX0; }
-//    public List<Block> createInRange(int minX, int maxX) {
-//        Block block = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
-//        block.setTag("ground");
-//
-//
-//    }
+        //לפי התיעוד במחלקהגשד
+        return groundHeightAtX0;
+
+//         float noise = (float) noiseGenerator.noise(x, Block.SIZE * 7); //
+//         TODO - itamar check why 7
+//        return groundHeightAtX0 + noise;
+
+    }
+
+    public List<Block> createInRange(int minX, int maxX) {
+
+        List<Block> blockList = new ArrayList<>();
+        minX -= (minX%Block.SIZE);
+        for (int x = minX; x <= maxX; x += Block.SIZE) {
+            double topY = Math.floor(groundHeightAt(x) / Block.SIZE) * Block.SIZE;
+            for (float y = windowDimensions.y(); y > topY;y -= Block.SIZE){
+//                Vector2 topLeftCorner = new Vector2(x, y);
+//                Block block = new Block(topLeftCorner, new
+//                RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+                Block block = new Block(Vector2.of(x, y),
+                        new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+                block.setTag(GROUND_TAG);
+                blockList.add(block);
+            }
+        }
+        return blockList;
+    }
 
 }
