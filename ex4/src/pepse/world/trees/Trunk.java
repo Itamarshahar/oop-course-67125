8c66a932b1e7ff43fc54144b1d492bfc54f3c437
Itamar Shahar <itamarshahar2@gmail.com>
2024-07-21 08:15:53 +0300

    ex4 - adding flora

--- ex4/src/pepse/world/trees/Trunk.java
+++ ex4/src/pepse/world/trees/Trunk.java
@@ -1,4 +1,52 @@
 package pepse.world.trees;
 
-public class branch {
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.world.Block;
+
+import java.awt.*;
+
+/**
+ * Represents the trunk of a tree in the game world.
+ * The trunk can respond to a jump action by changing its color.
+ */
+public class Trunk extends Block implements ResponsiveToJump {
+
+    public static final Color BASIC_TRUNK_COLOR = new Color(100, 50, 20);
+
+    public static final String TAG = "trunk";
+
+    /**
+     * Constructs a new Trunk instance.
+     *
+     * @param topLeftCorner The position of the top left corner of the trunk.
+     * @param renderable The renderable representing the trunk's appearance.
+     */
+    public Trunk(Vector2 topLeftCorner, Renderable renderable) {
+        super(topLeftCorner, renderable);
+        this.setTag(TAG); // TODO maybe redundant
+    }
+
+    /**
+     * Updates the state of the trunk.
+     *
+     * @param deltaTime The time elapsed since the last update.
+     */
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+    }
+
+    /**
+     * Responds to a jump action by changing the trunk's color.
+     */
+    @Override
+    public void onJump() {
+        // On jump of the avatar we will change the trunk color
+        Color newTrunkColor = ColorSupplier.approximateColor(Trunk.BASIC_TRUNK_COLOR);
+        RectangleRenderable newTrunkRenderer = new RectangleRenderable(newTrunkColor);
+        renderer().setRenderable(newTrunkRenderer);
+    }
 }
